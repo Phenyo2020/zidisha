480e8cff56c782170370bf96dc7f66f58d0ddd38
Phenyo2020 <62239075+Phenyo2020@users.noreply.github.com>
2020-08-22 13:04:37 +0200

    Add files via upload

---
+++ app/lang/en/comments.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'flash' => [
+        'post' => 'Your comments was posted',
+        'edit' => 'Your comment was edited',
+        'reply' => 'Your reply was posted',
+        'delete' => 'Your comment was deleted',
+        'translate' => 'Your comment was translated',
+        'file-not-valid' => 'The uploaded file is not valid',
+        'file-deleted' => 'Your File was Deleted',
+        'welcome' => 'Welcome to Zidisha!',
+        'already-invited' => 'Email :email is already been invited by you!',
+        'invite-success' => ':count Invite successfully sent!|:count Invites successfully sent!',
+        'facebook-no-account' => 'No Facebook account connected.',
+        'invalid-code' => 'The redemption code you entered is not valid. Please try again.',
+        'duplicate-code' => 'Sorry gift card redemption request could not be processed. Please contact to Administrator.',
+        'redeemed-code' => 'Sorry, this gift card has already been redeemed.',
+        'expired-code' => 'We are sorry, but this gift card expired on',
+        'redemption-success' => 'Your card is successfully Redeemed',
+        'borrower-join-email-sent' => 'A confirmation email is sent to your email address!'
+    ],
+    'actions' => [
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+        'translate' => 'Translate',
+        'reply' => 'Reply',
+        'share' => 'Share',
+        'show-original' => 'Display posting in original language',
+        'translated-by' => 'Translated by'
+    ],
+    'upload-file' => 'Select a file or image to upload.',
+    'add-more' => 'Add more'
+];
